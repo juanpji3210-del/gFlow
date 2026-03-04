80b3d8a9e1b18881c290024638d68d27c4385abc
nico1997 <misternico1997@gmail.com>
2026-03-03 20:59:41 -0500

    ADD sumar y restar

--- src/MiEmpresa/App.java
+++ src/MiEmpresa/App.java
@@ -1,4 +1,14 @@
 package MiEmpresa;
 
 public class App {
+
+    public static double sumar(double a, double b){
+        return a + b;
+    }
+
+    public static double restar(double a, double b){
+        return a - b;
+    }
+
+
 }
